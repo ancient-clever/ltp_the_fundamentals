da633c8ba70098ed496b6502032efccefe05bc27
ancient-clever <ancient-clever@outlook.com>
2019-09-14 18:28:37 +0300

    final

--- w4/a2.py
+++ w4/a2.py
@@ -8,6 +8,7 @@ def get_length(dna):
     >>> get_length('ATCG')
     4
     """
+    return len(dna)
 
 
 def is_longer(dna1, dna2):
@@ -21,6 +22,7 @@ def is_longer(dna1, dna2):
     >>> is_longer('ATCG', 'ATCGGA')
     False
     """
+    return len(dna1) > len(dna2)
 
 
 def count_nucleotides(dna, nucleotide):
@@ -33,6 +35,7 @@ def count_nucleotides(dna, nucleotide):
     >>> count_nucleotides('ATCTA', 'G')
     0
     """
+    return dna.count(nucleotide)
 
 
 def contains_sequence(dna1, dna2):
@@ -47,3 +50,66 @@ def contains_sequence(dna1, dna2):
     False
 
     """
+    return dna2 in dna1
+
+def is_valid_sequence(dna):
+    """ (str) -> bool
+
+    Return True if and only if the DNA sequence dna is valid
+    
+    >>> is_valid_sequence('ATCG')
+    True
+    >>> is_valid_sequence('ATcG')
+    False
+    >>> is_valid_sequence('ABCG')
+    False
+    """
+    result = True
+    for char in dna:
+        if char not in 'ATCG':
+            result = False
+    return result
+
+def insert_sequence(dna1, dna2, index):
+    """ (str, str, int) -> str
+
+    Return the DNA sequence obtained by inserting the second DNA sequence dna2
+    into the first DNA sequence dna1 at the given index.
+
+    >>> insert_sequence('CCGG', 'AT', 2)
+    CCATGG
+    >>> insert_sequence('CCGG', 'AT', 4)
+    CCGGAT
+    """
+    dna = dna1[:index] + dna2 + dna1[index:]
+    return dna
+
+def get_complement(nucleotide):
+    """ (str) -> str
+
+    Return the nucleotide's complement
+    
+    >>> get_complement('A')
+    T
+    >>> get_complement('G')
+    C
+    """
+    if nucleotide in 'AT':
+        return 'A' if 'T' in nucleotide else 'T'
+    if nucleotide in 'GC':
+        return 'G' if 'C' in nucleotide else 'C'
+
+def get_complementary_sequence(dna):
+    """ (str) -> str
+
+    Return the DNA sequence that is complementary to the given DNA sequence dna
+    
+    >>> get_complementary_sequence('ACGTACG')
+    'TGCATGC'
+    >>> get_complementary_sequence('ATCG')
+    'TAGC'
+    """
+    sequence = ''
+    for nucleotide in dna:
+        sequence = sequence + get_complement(nucleotide)
+    return sequence
